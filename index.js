884d2f602d2d05d925645f6283c0a3bc6870b76f
shubham yadav <129936985+shubham3185@users.noreply.github.com>
2023-10-21 09:39:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function changeimage(){
+    var img= document.getElementsByClassName("image")[0];
+    img.src="https://i.pinimg.com/736x/a0/c7/69/a0c769345687eb2638d2c6a17094116a.jpg";
+
+    var img= document.getElementsByClassName("image")[1];
+    img.src="https://i.pinimg.com/736x/48/4d/74/484d7408e03a9ac9bc8bc6841e343f0d.jpg";
+
+    var img= document.getElementsByClassName("image")[2];
+    img.src="https://static.wixstatic.com/media/fde259_92c07fbd561f4334af6ddaf918dde622~mv2.jpg/v1/fill/w_280,h_374,fp_0.46_0.48,q_90/fde259_92c07fbd561f4334af6ddaf918dde622~mv2.jpg";
+
+    var img= document.getElementsByClassName("image")[3];
+    img.src="https://cdn.cliqueinc.com/posts/309345/small-tattoos-309345-1694446899347-image.700x0c.jpg";
+
+    var img= document.getElementsByClassName("image")[4];
+    img.src="https://i.pinimg.com/1200x/d3/38/13/d33813d82c33684758e85991578066ce.jpg";
+
+    var img= document.getElementsByClassName("image")[5];
+    img.src="https://h6f9r7p4.rocketcdn.me/wp-content/uploads/2017/03/10597517_556102737850360_1285715768_n.jpg";
+
+    var img= document.getElementsByClassName("image")[6];
+    img.src="https://static.wixstatic.com/media/48ab72_25bdccb3b02e453d9c8b7feca39dcc8e~mv2.jpg/v1/fill/w_280,h_374,fp_0.46_0.33,q_90/48ab72_25bdccb3b02e453d9c8b7feca39dcc8e~mv2.jpg";
+
+    var img= document.getElementsByClassName("image")[7];
+    img.src="https://m.media-amazon.com/images/I/61HO70F-RGL._AC_UF1000,1000_QL80_.jpg";
+
+}
